d634e109c2614171036b6c219a77608027ca156c
Andrey Rakhmatullin <wrar@wrar.name>
2025-10-24 13:11:49 +0500

    Bump version: 0.4.1 → 0.5.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,7 +1,7 @@
 Changes
 =======
 
-0.5.0 (unreleased)
+0.5.0 (2025-10-24)
 ------------------
 
 * Added support for Python 3.12-3.14.
--- clear_html/__init__.py
+++ clear_html/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.4.1"
+__version__ = "0.5.0"
 from .clean import clean_node, cleaned_node_to_html, cleaned_node_to_text
 
 __all__ = ["clean_node", "cleaned_node_to_html", "cleaned_node_to_text"]
